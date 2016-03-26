23a44d754bf474f09330b0bbfda39f98de947870
mario <mario@drklpc>
2016-03-26 10:41:38 -0600

    Generacion de excel masivo - style

--- src/excelmasivo/ExcelMasivo.java
+++ src/excelmasivo/ExcelMasivo.java
@@ -18,6 +18,9 @@ import java.sql.Statement;
 import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Color;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.xssf.streaming.SXSSFCell;
 import org.apache.poi.xssf.streaming.SXSSFRow;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
@@ -50,6 +53,13 @@ public class ExcelMasivo {
         FileOutputStream out;
         int x = 0;
 
+        CellStyle cs = libro.createCellStyle();
+        cs.getFillForegroundColor();
+        Font f = libro.createFont();
+        //f.setBoldweight(Font.BOLDWEIGHT_BOLD);
+        f.setFontHeightInPoints((short) 12);
+        cs.setFont(f);
+
         try {
             Class.forName(driver);
             con = DriverManager.getConnection(url, user, pass);
@@ -62,6 +72,7 @@ public class ExcelMasivo {
                 for (int i = 1; i <= rsm.getColumnCount(); i++) {
                     //recorrer las columnas
                     celda = fila.createCell(i);
+                    celda.setCellStyle(cs);
                     celda.setCellValue(rs.getString(i));
                     //System.out.print(rs.getString(i));
                 }
